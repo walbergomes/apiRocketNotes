f277732a7f53d853c66f86be8ad540c6e11960bc
Walber <walberg.s48@gmail.com>
2024-07-18 18:41:08 -0300

    :white_check_mark: testtando e-mail em uso

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,5 +1,6 @@
 const UserCreateService = require("./UserCreateService");
 const UserRepositoryInMemory = require("./UserRepositoryInMemory")
+const AppError = require('../utils/AppError')
 
 describe("UserCreateService", () => {
   it("user should be create", async () => {
@@ -13,8 +14,34 @@ describe("UserCreateService", () => {
     const userCreateService = new UserCreateService(userRepositoryInMemory);
     const userCreated = await userCreateService.execute(user)
   
-    console.log(userCreated)
+    // console.log(userCreated)
   
     expect(userCreated).toHaveProperty("id")
   });
+
+  it("user not should be created with exist email", async () => {
+    const user1 = {
+      name: "User Test 1",
+      email: "user@test.com",
+      password: "123"
+    }
+
+    const user2 = {
+      name: "User Test 2",
+      email: "user@test.com",
+      password: "456"
+    }
+
+    const userRepository = new UserRepositoryInMemory()
+    const userCreateService = new UserCreateService(userRepository)
+
+    await userCreateService.execute(user1)
+    await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso."))
+    
+    // outra forma de fazer o teste acima
+    // expect(async () => {
+    //   await userCreateService.execute(user2)
+    // }).rejects.toEqual(new AppError("Este e-mail já está em uso."))
+
+  })
 })
