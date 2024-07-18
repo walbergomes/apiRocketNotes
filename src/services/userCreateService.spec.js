ddc2e8683b18f23749b0403500ad5c71da796c9e
Walber <walberg.s48@gmail.com>
2024-07-18 18:16:01 -0300

    :white_check_mark: explicando describe

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,20 +1,32 @@
 const UserCreateService = require("./UserCreateService");
 const UserRepositoryInMemory = require("./UserRepositoryInMemory")
 
-it("user should be create", async () => {
-  const user = {
-    name: "User Test",
-    email: "user@test.com",
-    password: "123",
-  };
-
-  const userRepositoryInMemory = new UserRepositoryInMemory()
-  const userCreateService = new UserCreateService(userRepositoryInMemory);
-  const userCreated = await userCreateService.execute(user)
+describe("UserCreateService", () => {
+  it("user should be create", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123",
+    };
   
+    const userRepositoryInMemory = new UserRepositoryInMemory()
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
+    const userCreated = await userCreateService.execute(user)
+  
+    console.log(userCreated)
+  
+    expect(userCreated).toHaveProperty("id")
+  });
 
-  console.log(userCreated)
 
-  expect(userCreated).toHaveProperty("id")
+  it("another test", () => {
+    expect(1).toBe(1)
+  });
+})
 
-});
+// O ideal é ter um "describe" por arquivo. Aqui tem 2 por questão de exemplo. 
+describe("NotesCreateService", () => {
+  it("create notes",  async () => {
+    expect(1).toBe(1)
+  })
+})
